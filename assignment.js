8e4fd1076b902c7b741c94e98d46226edfa750e9
Nihar Mondal <niharmondol9@gmail.com>
2020-10-30 16:36:03 +0600

    done

--- assignment.js
+++ assignment.js
@@ -17,15 +17,30 @@ function woodCalculator(chair, table, bed) {
 var result = (woodCalculator(5, 3, 2));
 console.log("I need" + " " + result, "cubic wood to make 'Five Chair, Three Table and Two Bed'.");
 
-function brickCalculator(height, stair) {
-    
-    var brick = 1000;
-    var height = brick * height;
-    var stair = height * stair;
-    var totalBrick = stair;
-         return totalBrick;
+function brickCalculator(floor) {
+    const height = [15, 12, 10]
+    if (floor <= 10) {
+        let firstTotalBrick = floor * height[0] * 1000;
+        return firstTotalBrick;
+    }
+    else if (floor <= 20) {
+        let top = floor - 10;
+        let down = floor - 10;
+        let totalBrick = top * height[0] + down * height[1]
+        let secondTotalBrick = totalBrick * 1000;
+        return secondTotalBrick;
+    } else {
+        let buildingTop = floor - 20;
+        let buildingMiddle = 10;
+        let buildingBottom = 10;
+        let totalBrick = buildingBottom * height[0] + buildingMiddle * height[1] + buildingTop * height[2];
+        let thirdTotalBrick = totalBrick * 1000;
+        return thirdTotalBrick;
+    }
+
 }
-console.log(brickCalculator(15,10))
+console.log(brickCalculator(30))
+
 
 
 function tinyFriend(name) {
